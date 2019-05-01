5e789a418f6d970aba8fabc45f5ef239d2fbb6e7
ezzatmohamed <3ezzatpk@gmail.com>
2019-05-01 22:40:36 +0200

    handling error !

--- app.js
+++ app.js
@@ -5,8 +5,20 @@ const commentApi = require('./api/routes/comments');
 
 app.use('/api/users',userApi);
 app.use('/api/comments',commentApi);
+app.use(function(req,res,next){
+    const error = new Error('Not FOund  !');
+    error.status = 404;
+    next(error);
+});
 
-
+app.use(function(error,req,res,next){
+    res.status(error.status || 500);
+    res.json({
+        error: {
+            message : error.message
+        }
+    });
+});
 
 
 module.exports = app;
